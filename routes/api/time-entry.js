76579f9d875b9f61b72c5fbebd72b95bbc7a89f3
Aishvarya verma <averma@clavax.us>
2020-02-04 20:29:43 +0530

    get time entry api

--- routes/api/time-entry.js
+++ routes/api/time-entry.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User');
 const auth = require('../../middleware/auth');
 const TimeEntry = require('../../models/TimeEntry');
 const { check, validationResult } = require('express-validator/check');
 
 /**
- * @Route       POST api/users
- * @desc        Register user
- * @access      Public
+ * @Route       GET api/time-entry
+ * @desc        Get all time entry of auth user
+ * @access      Private
+ */
+router.get('/', auth, async (req, res) => {
+    try {
+        const timeentries = await TimeEntry.find( { user: req.user.id });
+        
+        // Send response
+        res.status(200).json(timeentries);
+
+    } catch(err) {
+        // Sending error response
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+/**
+ * @Route       POST api/time-entry
+ * @desc        Create time entry
+ * @access      Private
  */
 router.post('/', [auth, [
     // Checking required fields
@@ -29,7 +47,8 @@ router.post('/', [auth, [
 
     // Find logged in user id
     const user = await User.findById(req.user.id).select('-password');
-    if(user) {
+    console.log(user)
+    if(!user) {
         // Sending error response
         return res.status(400).json({ errors: [{ msg: 'Bad request.' }] });
     }
